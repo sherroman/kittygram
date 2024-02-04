326fe4cd34c67887d619fa094dbcaa5d842e9f06
Роман Щербаков <sherromanrs@yandex.ru>
2024-02-05 01:00:49 +0300

    use generic views classes

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,8 +1,9 @@
-# urls.py
+# Обновлённый urls.py
 from django.urls import path
 
-from cats.views import APICat
+from cats.views import CatList, CatDetail
 
 urlpatterns = [
-    path('cats/', APICat.as_view()),
+    path('cats/', CatList.as_view()),
+    path('cats/<int:pk>/', CatDetail.as_view()),
 ]
